eec37b9684c5b04108587e26662daca252ccb83a
Rafael Miquelino <rafaelmiquelino@gmail.com>
2020-01-24 21:28:25 +0000

    Changed volume configuration

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ app = Flask(__name__)
 
 # A random secret used by Flask to encrypt session data cookies
 app.secret_key = os.environ['FLASK_SECRET_KEY']
-
+fileDir = os.environ['FILE_DIR'] 
+fileName = os.environ['FILE_NAME']
+filePath = fileDir + fileName 
 
 @app.route('/')
 def content():
-    text = open(os.environ['FILE_PATH'], 'r')
+    text = open(filePath,  'r')
     content = text.read()
     text.close()
     return render_template('content.html', text=content)
